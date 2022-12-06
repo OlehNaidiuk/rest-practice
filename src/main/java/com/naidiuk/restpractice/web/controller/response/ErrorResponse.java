9c9c110af4665a048961465c7f21e0b2ebb21225
Oleh Naidyuk <OlehNaidiuk>
2022-12-06 22:39:31 +0200

    Add exception handling for web layer

---
+++ src/main/java/com/naidiuk/restpractice/web/controller/response/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.naidiuk.restpractice.web.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
